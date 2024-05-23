9e56903757664cddcc2fca0a50dca564852c5fd7
Fernando del Caz <fernandodelcaz@gmail.com>
2024-05-23 15:28:08 +0200

    Refactor to csv source

--- java/src/test/java/ClassTest.java
+++ java/src/test/java/ClassTest.java
@@ -1,5 +1,6 @@
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.stream.Stream;
@@ -8,22 +9,18 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ClassTest {
 
-  public static Stream<Arguments> provideMovesAndExpectedResults() {
-    return Stream.of(
-      Arguments.of(Move.ROCK, Move.SCISSORS, GameStatus.PLAYER_ONE_WON),
-      Arguments.of(Move.SCISSORS, Move.ROCK, GameStatus.PLAYER_TWO_WON),
-      Arguments.of(Move.PAPER, Move.ROCK, GameStatus.PLAYER_ONE_WON),
-      Arguments.of(Move.ROCK, Move.PAPER, GameStatus.PLAYER_TWO_WON),
-      Arguments.of(Move.SCISSORS, Move.PAPER, GameStatus.PLAYER_ONE_WON),
-      Arguments.of(Move.PAPER, Move.SCISSORS, GameStatus.PLAYER_TWO_WON),
-      Arguments.of(Move.ROCK, Move.ROCK, GameStatus.DRAW),
-      Arguments.of(Move.PAPER, Move.PAPER, GameStatus.DRAW),
-      Arguments.of(Move.SCISSORS, Move.SCISSORS, GameStatus.DRAW)
-    );
-  }
-
   @ParameterizedTest
-  @MethodSource("provideMovesAndExpectedResults")
+  @CsvSource({
+    "ROCK, SCISSORS, PLAYER_ONE_WON",
+    "SCISSORS, ROCK, PLAYER_TWO_WON",
+    "PAPER, ROCK, PLAYER_ONE_WON",
+    "ROCK, PAPER, PLAYER_TWO_WON",
+    "SCISSORS, PAPER, PLAYER_ONE_WON",
+    "PAPER, SCISSORS, PLAYER_TWO_WON",
+    "ROCK, ROCK, DRAW",
+    "PAPER, PAPER, DRAW",
+    "SCISSORS, SCISSORS, DRAW"
+  })
   void testRockPaperScissors(Move playerOneMovement, Move playerTwoMovement, GameStatus expectedGameStatus) {
     RockPaperScissors rockPaperScissors = new RockPaperScissors();
     GameStatus gameStatus = rockPaperScissors.move(playerOneMovement, playerTwoMovement);
