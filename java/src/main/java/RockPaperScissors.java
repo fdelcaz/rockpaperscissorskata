87f65ab87d5a3eb42061a566aa4860113f976ac3
Fernando del Caz <fernandodelcaz@gmail.com>
2024-05-23 15:29:29 +0200

    Remove unneeded else·

--- java/src/main/java/RockPaperScissors.java
+++ java/src/main/java/RockPaperScissors.java
@@ -3,12 +3,12 @@ public class RockPaperScissors {
 
     if (playerOneMovement.equals(playerTwoMovement))
       return GameStatus.DRAW;
-    else{
-      return switch (playerOneMovement) {
-        case ROCK -> playerTwoMovement.equals(Move.SCISSORS) ? GameStatus.PLAYER_ONE_WON : GameStatus.PLAYER_TWO_WON;
-        case PAPER -> playerTwoMovement.equals(Move.ROCK) ? GameStatus.PLAYER_ONE_WON : GameStatus.PLAYER_TWO_WON;
-        case SCISSORS -> playerTwoMovement.equals(Move.PAPER) ? GameStatus.PLAYER_ONE_WON : GameStatus.PLAYER_TWO_WON;
-      };
-    }
+
+    return switch (playerOneMovement) {
+      case ROCK -> playerTwoMovement.equals(Move.SCISSORS) ? GameStatus.PLAYER_ONE_WON : GameStatus.PLAYER_TWO_WON;
+      case PAPER -> playerTwoMovement.equals(Move.ROCK) ? GameStatus.PLAYER_ONE_WON : GameStatus.PLAYER_TWO_WON;
+      case SCISSORS -> playerTwoMovement.equals(Move.PAPER) ? GameStatus.PLAYER_ONE_WON : GameStatus.PLAYER_TWO_WON;
+    };
+
   }
 }
