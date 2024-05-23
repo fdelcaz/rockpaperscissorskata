db4925c058cc13f94a153cab2b90d56264822461
Fernando del Caz <fernandodelcaz@gmail.com>
2024-05-23 14:54:37 +0200

    Refactor switch to enhanced version

--- java/src/main/java/RockPaperScissors.java
+++ java/src/main/java/RockPaperScissors.java
@@ -4,16 +4,12 @@ public class RockPaperScissors {
     if (playerOneMovement.equals(playerTwoMovement))
       return GameStatus.DRAW;
     else{
-      switch (playerOneMovement) {
-        case ROCK:
-          return playerTwoMovement.equals(Move.SCISSORS) ? GameStatus.PLAYER_ONE_WON : GameStatus.PLAYER_TWO_WON;
-        case PAPER:
-          return playerTwoMovement.equals(Move.ROCK) ? GameStatus.PLAYER_ONE_WON : GameStatus.PLAYER_TWO_WON;
-        case SCISSORS:
-          return playerTwoMovement.equals(Move.PAPER) ? GameStatus.PLAYER_ONE_WON : GameStatus.PLAYER_TWO_WON;
-        default:
-          return GameStatus.DRAW;
-      }
+      return switch (playerOneMovement) {
+        case ROCK -> playerTwoMovement.equals(Move.SCISSORS) ? GameStatus.PLAYER_ONE_WON : GameStatus.PLAYER_TWO_WON;
+        case PAPER -> playerTwoMovement.equals(Move.ROCK) ? GameStatus.PLAYER_ONE_WON : GameStatus.PLAYER_TWO_WON;
+        case SCISSORS -> playerTwoMovement.equals(Move.PAPER) ? GameStatus.PLAYER_ONE_WON : GameStatus.PLAYER_TWO_WON;
+        default -> GameStatus.DRAW;
+      };
     }
   }
 }
